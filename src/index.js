06799c7d57d539d8200a08e969c2f6c459093c09
John W Kerns <jkerns@packetsar.com>
2022-06-26 09:26:04 -0700

    Added parse and clear buttons

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 /* global $ */
 const neighbors = require('./neighbors')
 
-$(document).ready(function () {
-  // const table = $('#neighborTable').DataTable()
+function drawNeighbors () {
   const table = $('#neighborTable').DataTable({ retrieve: true })
+  const neighborData = $('#neighborData')
+  table.clear().draw()
+  const neighborObjs = neighbors.getNeighbors(neighborData.val())
+  for (let i = 0; i < neighborObjs.length; i++) {
+    neighborObjs[i].num = i + 1
+    table.row.add(neighborObjs[i])
+  };
+  table.draw()
+};
+
+$(document).ready(function () {
+  // Set a trigger for keyup on the neighborData field
   $('#neighborData').keyup(function () {
-    table.clear().draw()
-    const neighborObjs = neighbors.getNeighbors($(this).val())
-    for (let i = 0; i < neighborObjs.length; i++) {
-      neighborObjs[i].num = i + 1
-      table.row.add(neighborObjs[i])
-    };
-    table.draw()
+    drawNeighbors()
+  })
+  // Set a trigger for clicking on the "Parse" button
+  $('#parseNeighbors').click(function () {
+    drawNeighbors()
+    $('#neighbor_search').focus()
+  })
+  // Set a trigger for a click on the "Clear" button
+  $('#clearNeighborData').click(function () {
+    const neighborData = $('#neighborData')[0]
+    neighborData.value = ''
+    drawNeighbors()
+    neighborData.focus()
   })
 })
