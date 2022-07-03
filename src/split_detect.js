e71575ecb12940a8d7fd97252c36044d42ab1a95
John W Kerns <jkerns@packetsar.com>
2022-07-03 11:11:10 -0700

    Added getShortBlocks

--- src/split_detect.js
+++ src/split_detect.js
@@ -1,4 +1,33 @@
 
+function getShortBlocks (block) {
+  /*
+  Regex to match "show cdp neighbors" or "show lldp neighbors" output, but not
+  the "detail" output.
+
+  Explanation:
+    Commands without the "detail" keyword produce headers like
+    ("Device ID      Local Interfce...") for the table data which can be
+    matched. BUT, on NXOS, the output of "show lldp neighbors detail" ALSO
+    produces a stupid header for no obvious reason, so I have to exclude that
+    type of output in the regex. Luckily, the NXOS LLDP "detail" output
+    includes an empty line right after the useless header. This regex exploits
+    that extra empty line and excludes any output which has it.
+
+  Regex Breakdown:
+    - Match "Device ID" or "Device-ID"
+    - Match an unknown number of spaces between "ID" and "Local"
+    - Match "Intf" or "Intrfce" right after "Local"
+    - Match (non-greedy) anything except a new line up until "Port ID"
+      - "Port ID" is the last column in the header
+    - Match a single newline after "Port ID", then next character must be
+        something other than a newline
+    - Non-greedily match all characters until either:
+      1. "Total entries displayed" is encountered, or
+      2. "Total cdp entries displayed" is encountered
+  */
+  return block.match(/Device( |-)ID[ ]*Local Int(r)?f(ce)?[^\n]*?Port ID\n[^\n].*?Total (cdp )?entries displayed/gs)
+};
+
 function split (block) {
   const re = /-----*|\n\n\n/
   return block.split(re)
@@ -35,6 +64,7 @@ function cleanArray (blockObjArray) {
 };
 
 module.exports = {
+  getShortBlocks,
   split,
   detect,
   splitDetect,
---
+++ src/tests/mixedshort_split.test.js
@@ -0,0 +1,35 @@
+const split = require('../split_detect')
+const cdpIos = require('./samples/cdpshort_ios')
+const cdpIosxe = require('./samples/cdpshort_iosxe')
+const cdpNxos = require('./samples/cdpshort_nxos')
+const lldpIos = require('./samples/lldpshort_ios')
+const lldpIosxe = require('./samples/lldpshort_iosxe')
+const lldpNxos = require('./samples/lldpshort_nxos')
+const nxosDetail = require('./samples/lldp_nxos_empty_blocks')
+
+const sampleArray = [
+  cdpIos,
+  cdpIosxe,
+  cdpNxos,
+  lldpIos,
+  lldpIosxe,
+  lldpNxos
+]
+
+test('Check all short samples render only one match', () => {
+  sampleArray.forEach(function (sample) {
+    expect(split.getShortBlocks(sample.sample).length).toBe(1)
+  })
+})
+
+test('Check appended samples render a match for each additional', () => {
+  let sample = ''
+  for (let i = 0; i < sampleArray.length; i++) {
+    sample = sample + sampleArray[i].sample
+    expect(split.getShortBlocks(sample).length).toBe(i + 1)
+  }
+})
+
+test('Check that NXOS "show lldp neighbor detail" does not match', () => {
+  expect(split.getShortBlocks(nxosDetail.fourteenBlocks)).toBe(null)
+})
