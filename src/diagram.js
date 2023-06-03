848be106acc00bd731c2a152c8fd916c77dc394d
John W Kerns <jkerns@packetsar.com>
2023-06-03 11:05:28 -0700

    Tweaks and comments

--- src/diagram.js
+++ src/diagram.js
@@ -104,27 +104,30 @@ function makePaper (element) {
       useLinkTools: false
     }
   })
-  
-  // panning functionality
-  var dragStartPosition = null;
-  paper.on("blank:pointerdown", function (evt, x, y) {
-    var scale = paper.scale()
-    
-    dragStartPosition = { x: x * scale.sx, y: y * scale.sy };
+  // ########## Add paper panning functionality
+  let dragStartPosition = null // Initialize the drag position
+  // When mouse clicks on empty area (not a cell)
+  paper.on('blank:pointerdown', function (evt, x, y) {
+    // Set the start position for dragging/translating the paper
+    const scale = paper.scale()
+    dragStartPosition = { x: x * scale.sx, y: y * scale.sy }
   })
-
-  paper.on("cell:pointerup blank:pointerup", function (cellView, x, y) {
-      dragStartPosition = null;
+  // When the mouse click lets up, reset the drag position
+  paper.on('cell:pointerup blank:pointerup', function (cellView, x, y) {
+    dragStartPosition = null
   })
-
-  $("#diagram").mousemove(function (event) {
+  // When the mouse moves across the paper
+  $('#diagram').mousemove(function (event) {
+    // If the drag position has been set, then mouse is clicked on and
+    //     holding a blank area of the paper.
     if (dragStartPosition != null) {
-        paper.translate(
-            event.offsetX - dragStartPosition.x,
-            event.offsetY - dragStartPosition.y);
+      // Move the paper in relation to the dragStartPosition
+      paper.translate(
+        event.offsetX - dragStartPosition.x,
+        event.offsetY - dragStartPosition.y
+      )
     }
-  });
-  
+  })
   // Create a adjustGraphVertices() function to wrap the
   //   multipleLinks.adjustVertices function.
   // The new adjustGraphVertices() function automatically passes in the graph
